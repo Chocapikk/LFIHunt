234fcb76328059b13546f1760a5f7d4e8a90ad71
Balgogan <balgogan@protonmail.com>
2023-06-19 18:33:36 +0200

    Fixed ConnectionError in new feature on LFIChecker

--- core/LFIChecker.py
+++ core/LFIChecker.py
@@ -79,10 +79,14 @@ class LFIChecker:
         shared_results = []
         stop_signal = False
 
-        for param in params:
-            random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
-            response = requests.get(self.url, params={param: random_string}, verify=False)
-            response_lengths.append(len(response.content))
+        try:
+            for param in params:
+                random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+                response = requests.get(self.url, params={param: random_string}, verify=False)
+                response_lengths.append(len(response.content))
+        except requests.exceptions.ConnectionError:
+            console.print("[bold red]Request Failed (possible WAF block)...[/bold red]")
+            return False, None      
 
         def send_request(file_path, file_regex, param_name):
             nonlocal shared_results, connection_error_count, stop_signal
